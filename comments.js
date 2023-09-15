e41d3af51b259f565b834e8b3be5a1e50b33bc98
Ghadisi <92534399+Ghadisi@users.noreply.github.com>
2023-09-15 14:40:24 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,11 @@
+//create web server
+//create web server for comment
+//============================================
+
+// import module
+const express = require('express');
+const router = express.router();
+const commentController = require('...controllers/comment');
+
+
+// handle request ........etc
